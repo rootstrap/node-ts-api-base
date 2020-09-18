1dd604a75935d3df2e6b54888682ad19f5c63f84
Alexis <alezuv@gmail.com>
2020-09-17 22:12:07 -0300

    Lint fixes

--- src/services/jwt.service.ts
+++ src/services/jwt.service.ts
@@ -5,7 +5,7 @@ import { JWT_SECRET, ACCESS_TOKEN_LIFE } from '@config';
 export const createJWT = (user: User): Promise<string> => {
   return new Promise((resolve, reject) => {
     try {
-      let token = jwt.sign(
+      const token = jwt.sign(
         { data: { userId: user.id, email: user.email } },
         JWT_SECRET || '',
         { expiresIn: ACCESS_TOKEN_LIFE || 360 }
@@ -13,19 +13,21 @@ export const createJWT = (user: User): Promise<string> => {
       resolve(token);
     } catch (error) {
       console.log('decode error: ', error);
-      reject(false);
+      reject(new Error('Error creating JWT'));
     }
   });
 };
 
-export const decodeJWT = (token: string): Promise<string | { [key: string]: any } | null> => {
+export const decodeJWT = (
+  token: string
+): Promise<string | { [key: string]: any } | null> => {
   return new Promise((resolve, reject) => {
     try {
-      let decoded = jwt.decode(token);
+      const decoded = jwt.decode(token);
       resolve(decoded);
     } catch (error) {
       console.log('decode error: ', error);
-      reject(false);
+      reject(new Error('Error decoding JWT'));
     }
   });
 };
