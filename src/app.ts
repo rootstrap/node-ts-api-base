be81815efda92547849e56a351f94632c2bab563
Alex Fradiani <a.fradiani@gmail.com>
2020-10-05 16:51:51 +0200

    conditional middleware based on env

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,23 @@ import authorizationChecker from '@middlewares/auth.middleware';
 // required by routing-controllers
 useContainer(Container);
 
+// middlewares to load with the app:
+let middlewares: string[] = ['auth.middleware.ts'];
+
+// skip logging during testing
+if (process.env.ENVIRONMENT !== 'test') {
+  middlewares.push('logging.middleware.ts');
+}
+
+const prefix = `${__dirname}/middlewares/`;
+middlewares = middlewares.map(value => `${prefix}${value}`);
+
 const app = createExpressServer({
   routePrefix: '/api/v1',
   cors: true,
   authorizationChecker: authorizationChecker,
   controllers: [__dirname + '/controllers/*.ts'],
-  middlewares: [__dirname + '/middlewares/*.ts'],
+  middlewares,
   interceptors: [__dirname + '/interceptors/*.ts']
 });
 
