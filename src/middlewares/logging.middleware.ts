946ee7f0ad975c9e3d99a1af26b0040fc1b405ea
Alexis <alezuv@gmail.com>
2020-10-06 10:28:54 -0300

    Add moragn to log

--- src/middlewares/logging.middleware.ts
+++ src/middlewares/logging.middleware.ts
@@ -1,9 +1,14 @@
 import { Middleware, ExpressMiddlewareInterface } from 'routing-controllers';
+import morgan from 'morgan';
 
 @Middleware({ type: 'before' })
 export class LoggingMiddleware implements ExpressMiddlewareInterface {
-  use(request: any, _response: any, next: (err?: any) => any) {
-    console.log(`${request.method} ${request.path}`);
-    next();
+  use(request: any, response: any, next: (err?: any) => any) {
+    const logger = morgan('dev', {
+      skip: () => {
+        return process.env.ENVIRONMENT === 'test';
+      }
+    });
+    logger(request, response, next);
   }
 }
