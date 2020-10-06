1206c3891fa2cb11fda570c2b93ede775129cdb9
Alexis <alezuv@gmail.com>
2020-10-06 10:37:10 -0300

    Use config variable

--- src/middlewares/logging.middleware.ts
+++ src/middlewares/logging.middleware.ts
@@ -1,12 +1,13 @@
 import { Middleware, ExpressMiddlewareInterface } from 'routing-controllers';
 import morgan from 'morgan';
+import { ENVIRONMENT } from '@config';
 
 @Middleware({ type: 'before' })
 export class LoggingMiddleware implements ExpressMiddlewareInterface {
   use(request: any, response: any, next: (err?: any) => any) {
     const logger = morgan('dev', {
       skip: () => {
-        return process.env.ENVIRONMENT === 'test';
+        return ENVIRONMENT === 'test';
       }
     });
     logger(request, response, next);
