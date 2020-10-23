9e3d25e0e7baa2790eb7ab0bd3c82677b309e91c
Diego Barral <diebarral@users.noreply.github.com>
2020-10-23 12:41:15 -0300

    Add middleware to prevent DoS attacks (Rate limit) (#49)
    * Setup rate limiter
    
    * changes to avoid ERR_HTTP_HEADERS_SENT
    
    * Change format to routing-controller middleware
    
    * Use express middleware
    
    * Add test
    
    Co-authored-by: pablanco <3788239+pablanco@users.noreply.github.com>
    Co-authored-by: Alexis Zucco [Macbook] <alexis@rootstrap.com>
    Co-authored-by: Alexis <alezuv@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,29 @@
 import 'reflect-metadata'; // this shim is required
-import { createExpressServer, useContainer } from 'routing-controllers';
+import express from 'express';
+import { useContainer, useExpressServer } from 'routing-controllers';
 import { Container } from 'typedi';
 import Auth from '@middlewares/auth.middleware';
+import rateLimit from 'express-rate-limit';
+import { RATE_LIMIT_MAX_REQUESTS, RATE_LIMIT_WINDOW } from '@config';
 
 // required by routing-controllers
 useContainer(Container);
 
-const app = createExpressServer({
+// Create express server
+const app: express.Express = express();
+
+// Setup express middlewares
+app.use(rateLimit({
+  windowMs: Number.parseInt(RATE_LIMIT_WINDOW || '900000'),
+  max: Number.parseInt(RATE_LIMIT_MAX_REQUESTS || '10'),
+  message: {
+    status: 429,
+    message: 'Too many requests, please try again later.'
+  }
+}));
+
+// Wrap server with routing-controllers
+useExpressServer(app, {
   routePrefix: '/api/v1',
   cors: true,
   authorizationChecker: Auth.checker,
