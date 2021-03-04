ac5edf6dfe85ad153f3a400c34349061b29a73f9
Nicolas Santos Reyes <nicolassantosreyes@Nicolass-MacBook-Pro.local>
2021-03-04 11:16:06 -0300

    Add ENV on logging middleware

--- src/middlewares/logging.middleware.ts
+++ src/middlewares/logging.middleware.ts
@@ -1,13 +1,13 @@
 import { Middleware, ExpressMiddlewareInterface } from 'routing-controllers';
 import morgan from 'morgan';
-import { ENVIRONMENT } from '@config';
+import { ENV } from '@config';
 
 @Middleware({ type: 'before' })
 export class LoggingMiddleware implements ExpressMiddlewareInterface {
   use(request: any, response: any, next: (err?: any) => any) {
     const logger = morgan('dev', {
       skip: () => {
-        return ENVIRONMENT === 'test';
+        return ENV === 'test';
       }
     });
     logger(request, response, next);
