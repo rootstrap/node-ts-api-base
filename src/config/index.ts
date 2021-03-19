1af6e271a4a98e0411d8133d3a8855fe0c1d2d9a
Alexis Zucco <alexis@rootstrap.com>
2021-03-19 15:38:28 -0300

    Replace ormconfig, remove redundant commands & add current env constants (#374)
    * Replace ormconfig, remove redundant commands & add current env constants
    
    * Added posttypeorm script to prettify migrations
    
    * Added type checking to TYPEORM_TYPE env variable

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
-const dotenv = require('dotenv');
+import { config } from 'dotenv';
 
-dotenv.config({ path: `.env.${process.env.ENV || 'dev'}` });
+config({ path: `.env.${process.env.ENV || 'dev'}` });
 
 // If .env wasn't provided then exit
 if (!process.env.PORT) {
@@ -8,9 +8,22 @@ if (!process.env.PORT) {
   process.exit(1);
 }
 
+export const PRODUCTION_ENV = process.env.ENV === 'prod';
+export const TESTING_ENV = process.env.ENV === 'test';
+export const CI_ENV = process.env.ENV === 'ci';
+
 export const {
   ENV,
   PORT,
+  TYPEORM_PORT,
+  TYPEORM_HOST,
+  TYPEORM_TYPE,
+  TYPEORM_USERNAME,
+  TYPEORM_PASSWORD,
+  TYPEORM_DATABASE,
+  TYPEORM_SYNCHRONIZE,
+  TYPEORM_LOGGING,
+  TYPEORM_MIGRATIONS_RUN,
   JWT_SECRET,
   ACCESS_TOKEN_LIFE,
   RATE_LIMIT_WINDOW,
--- src/database/migrations/1606158886329-CreateUserEntity.ts
+++ src/database/migrations/1606158886329-CreateUserEntity.ts
@@ -2,44 +2,47 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateUserEntity1606158886329 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.createTable(new Table({
-      name: 'user',
-      columns: [
-        {
-          name: 'id',
-          type: 'int',
-          isPrimary: true,
-          isGenerated: true,
-          generationStrategy: 'increment'
-        },
-        {
-          name: 'firstName',
-          type: 'varchar'
-        },
-        {
-          name: 'lastName',
-          type: 'varchar'
-        },
-        {
-          name: 'email',
-          type: 'varchar'
-        },
-        {
-          name: 'password',
-          type: 'varchar'
-        },
-        {
-          name: 'created_at',
-          type: 'timestamp',
-          default: 'now()'
-        },
-        {
-          name: 'updated_at',
-          type: 'timestamp',
-          default: 'now()'
-        }
-      ]
-    }), true);
+    await queryRunner.createTable(
+      new Table({
+        name: 'user',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment'
+          },
+          {
+            name: 'firstName',
+            type: 'varchar'
+          },
+          {
+            name: 'lastName',
+            type: 'varchar'
+          },
+          {
+            name: 'email',
+            type: 'varchar'
+          },
+          {
+            name: 'password',
+            type: 'varchar'
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()'
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()'
+          }
+        ]
+      }),
+      true
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
