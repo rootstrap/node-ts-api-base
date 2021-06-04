ffad50588ad5c3cb35a803cb675ea9aa88ceb282
agustinpurita <agustinpurita27@hotmail.com>
2021-06-04 10:28:21 -0300

    add types for builders

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,11 +1,13 @@
 import * as sgTransport from 'nodemailer-sendgrid-transport';
-import nodemailer from 'nodemailer';
+import nodemailer, { Transporter } from 'nodemailer';
 import aws from 'aws-sdk';
 import { Service } from 'typedi';
 import { AuthSg, ISgOptions, IEmail } from '@interfaces/email.interface';
 import { transpoterMapper } from '@utils/emailPlatformMap';
 import { SENDGRID_API_KEY, SENDGRID_API_USER } from '@config';
 import { SESService } from '@services/ses.service';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
+import SESTransport from 'nodemailer/lib/ses-transport';
 
 @Service()
 export class EmailService {
@@ -15,17 +17,16 @@ export class EmailService {
     };
   }
 
-  static buildSendGridTransport() {
+  static buildSendGridTransport(): Transporter<SMTPTransport.SentMessageInfo> {
     const credentials = {
       api_user: SENDGRID_API_USER,
       api_key: SENDGRID_API_KEY as string
     };
-    return nodemailer.createTransport(
-      sgTransport(this.getSgOptions(credentials))
-    );
+    const options = sgTransport(this.getSgOptions(credentials));
+    return nodemailer.createTransport(sgTransport(options));
   }
 
-  static buildSesTransport() {
+  static buildSesTransport(): Transporter<SESTransport.SentMessageInfo> {
     const sesService = new SESService();
     const ses = sesService.createSES();
     return nodemailer.createTransport({
@@ -33,7 +34,7 @@ export class EmailService {
     });
   }
 
-  sendEmail(email: IEmail, emailPlatform) {
+  sendEmail(email: IEmail, emailPlatform: string) {
     const transporter = transpoterMapper[emailPlatform];
     return new Promise((resolve, reject) => {
       transporter.sendMail(email, (err, res) => {
--- src/utils/emailPlatformMap.ts
+++ src/utils/emailPlatformMap.ts
@@ -1,6 +1,14 @@
+import { EmailPlatforms } from '@constants/emailPlatforms';
 import { EmailService } from '@services/email.service';
+import { Transporter } from 'nodemailer';
+import SESTransport from 'nodemailer/lib/ses-transport';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
 
-export const transpoterMapper = {
-  'SENDGRID': EmailService.buildSendGridTransport,
-  'SES': EmailService.buildSesTransport
+export const transpoterMapper: {
+  [k in EmailPlatforms]: () => Transporter<
+    SMTPTransport.SentMessageInfo | SESTransport.SentMessageInfo
+  >;
+} = {
+  SENDGRID: EmailService.buildSendGridTransport,
+  SES: EmailService.buildSesTransport
 };
