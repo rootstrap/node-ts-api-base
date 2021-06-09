216421fcdc54c310660a8fc04bdae81d9930c3ec
alexis <alezuv@gmail.com>
2021-06-09 10:26:48 -0300

    Unnecessary key, value assginment

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ const routingControllersOptions: any = {
   cors: true,
   authorizationChecker: AuthorizationService.getInstance().authorizationChecker,
   controllers,
-  middlewares: middlewares,
+  middlewares,
   interceptors: []
 };
 
