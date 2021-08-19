ef6903d12827b50b13931ac3273c7538e7d4ab72
Emiliano Mazzaglia <emiliano.mazzaglia@rootstrap.com>
2021-08-19 16:13:26 -0300

    Remove unncessary comments

--- src/exception/base.error.ts
+++ src/exception/base.error.ts
@@ -1,9 +1,5 @@
 import { HttpStatusCode } from '@constants/httpStatusCode';
 
-// eslint-disable-next-line
-//Information on captureStackTrace https://stackoverflow.com/questions/59625425/understanding-error-capturestacktrace-and-stack-trace-persistance
-// eslint-disable-next-line
-// Information on setPrototypeOf https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Object/setPrototypeOf
 export class BaseError extends Error {
   public readonly name: string;
   public readonly httpCode: HttpStatusCode;
